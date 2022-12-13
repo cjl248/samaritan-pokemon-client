e620715d6e9f6567d0d03221440bbf6484159468
github cjl248 <github cjl248@cornell.edu>
2022-12-13 12:39:27 -0500

    Returning main Home landing and wrapper component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './App.scss';
+import Home from './components/home/Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>"RESET"</h1>
+      <Home></Home>
     </div>
   );
 }
